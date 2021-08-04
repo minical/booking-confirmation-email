233d46d6e3e025b35537d1de504e280c3e10a198
LAPTOP-NRDN6ALL\priya <pooja.quazma@gmail.com>
2021-08-04 12:22:29 +0530

    code cleanup and language translation added

---
+++ language/portuguese/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['booking-confirmation-email']['booking_confirmation_email_settings'] = 'Configurações de e-mail de confirmação de reserva';
+$lang['booking-confirmation-email']['Thanks for booking your stay at'] = 'Obrigado por reservar a sua estadia em';
+$lang['booking-confirmation-email']['Guest Information'] = 'Informação para hóspedes';
+$lang['booking-confirmation-email']['Information goes here'] = 'As informações vão aqui';
+$lang['booking-confirmation-email']['Reservation Information'] = 'Informação de reserva';
+$lang['booking-confirmation-email']['Contact Information'] = 'Informações de contato';
+$lang['booking-confirmation-email']['Phone'] = 'Telefone';
+$lang['booking-confirmation-email']['Fax'] = 'Fax';
+$lang['booking-confirmation-email']['Email'] = 'Email';
+$lang['booking-confirmation-email']['Website'] = 'Local na rede Internet';
+$lang['booking-confirmation-email']['Reservation Policies'] = 'Políticas de reserva';
+$lang['booking-confirmation-email']['Estimated total before tax and service charges'] = 'Total estimado antes de impostos e taxas de serviço';
+$lang['booking-confirmation-email']['Update'] = 'Atualizar';
+
+?>
