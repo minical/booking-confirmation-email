f1aa2b1baf76beabb25aaf9c616424bb9f7d2bf2
LAPTOP-BM27UMG1\mradu <mradul.jain90@gmail.com>
2022-02-17 14:43:20 +0530

    set booking confirmation email as open source extension

--- config/config.php
+++ config/config.php
@@ -8,6 +8,8 @@ $config = array(
     "image_name" => "file-1.png",
     "setting_link" => "email/booking_confirmation_email",
     "is_vendor_module" => true,
+    "categories" => array("check_in_automation"),
+    "is_open_source" => true,
     "marketplace_product_link" => "http://marketplace.minical.io/product/booking-email/"
 
 );
